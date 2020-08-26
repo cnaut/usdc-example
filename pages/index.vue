db703229506cd7e4667ea75725ced2043b339fb2
Charles Naut <cpn585@gmail.com>
2020-08-25 22:45:26 -0400

    Move more components from example payments app

--- pages/index.vue
+++ pages/index.vue
@@ -13,6 +13,71 @@
           label="Card Number"
           :disabled="loading"
         />
+
+        <v-row>
+          <v-col cols="12" md="6">
+            <CVVInput
+              v-model="formData.cardData.cvv"
+              :rules="[rules.isNumber]"
+              default-label="CVV"
+              :disabled="loading"
+            />
+          </v-col>
+          <v-col cols="12" md="6">
+            <ExpiryInput
+              v-model="formData.cardData.expiry"
+              :labels="expiryLabels"
+              :required="true"
+            />
+          </v-col>
+        </v-row>
+
+        <div class="my-4 subtitle-1 black--text">
+          Billing Details
+        </div>
+
+        <v-text-field
+          v-model="formData.cardData.name"
+          :rules="[rules.required]"
+          hint="Full name of the card holder"
+          label="Cardholder name"
+          :disabled="loading"
+        />
+
+        <v-text-field
+          v-model="formData.cardData.line1"
+          :rules="[rules.required]"
+          label="Address Line 1"
+          :disabled="loading"
+        />
+
+        <v-text-field
+          v-model="formData.cardData.line2"
+          label="Address Line 2"
+        />
+
+        <v-text-field
+          v-model="formData.cardData.postalCode"
+          :rules="[rules.required]"
+          label="Postalcode"
+          :disabled="loading"
+        />
+
+        <v-text-field
+          v-model="formData.cardData.city"
+          :rules="[rules.required]"
+          label="City"
+          :disabled="loading"
+        />
+
+        <v-text-field
+          v-model="formData.cardData.district"
+          :rules="[rules.required]"
+          label="District"
+          :disabled="loading"
+          hint="State / County / Province / Region portion of the address. US and Canada use the two-letter code for the subdivision"
+        />
+
     </v-flex>
   </v-layout>
 </template>
@@ -20,6 +85,8 @@
 <script lang="ts">
 import AmountInput from '~/components/AmountInput.vue'
 import CardInput from '~/components/CardInput.vue'
+import CVVInput from '~/components/CVVInput.vue'
+import ExpiryInput from '~/components/ExpiryInput.vue'
 
 interface FormData {
   amount: string
@@ -45,11 +112,14 @@ interface FormData {
 export default {
   components: {
     AmountInput,
-    CardInput
+    CardInput,
+    CVVInput,
+    ExpiryInput
   },
   data: function() {
     return {
       validForm: false,
+      loading: false,
       formData: {
         amount: '0.00',
         cardData: {
@@ -69,6 +139,15 @@ export default {
           phoneNumber: '',
           email: '',
         },
+      },
+      rules: {
+        isNumber: (v: string) =>
+          v === '' || !isNaN(parseInt(v)) || 'Please enter valid number',
+        required: (v: string) => !!v || 'Field is required',
+      },
+      expiryLabels: {
+        month: 'Expiry Month',
+        year: 'Expiry Year',
       }
     }
   }
